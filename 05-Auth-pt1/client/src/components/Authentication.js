379161771b64ed1db161b3b75bcf9ecc4bf5319c
Princeton J. Rose <princetonjrose@gmail.com>
2023-09-22 13:18:47 -0400

    finished lecture, did terrible... 😭

--- 05-Auth-pt1/client/src/components/Authentication.js
+++ 05-Auth-pt1/client/src/components/Authentication.js
@@ -2,39 +2,77 @@ import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import styled from "styled-components";
 
+const initialState = {
+  username: '',
+  password: ''
+}
 
 function Authentication({ updateUser }) {
   const [signUp, setSignUp] = useState(false)
+  const [ formState, setFormState ] = useState( initialState )
+
+  const [ errors, setErrors ] = useState( null )
   const history = useHistory()
+
+  const changeFormState = event => {
+    const { name, value } = event.target
+    setFormState( { ...formState, [ name ]: value } )
+  }
 
   const handleClick = () => setSignUp((signUp) => !signUp)
   // 3.✅ Finish building the authentication form
     // 3.3 on submit create a POST. 
-      // 3.4 There is a button that toggles the component between login and sign up.
+  const postToLoginOrSignup = event => {
+    event.preventDefault()
+
+    const postRequest = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      },
+      body: JSON.stringify( formState )
+    }
+
+    // 3.4 There is a button that toggles the component between login and sign up.
       // if signUp is true use the path '/signup' else use '/login' (we will be writing login soon)
-      // Complete the post and test our '/signup' route 
+    fetch( signUp ? '/signup' : '/login', postRequest )
+    .then( r => r.json() )
+    .then( userData => {
+      if ( userData.errors )
+        setErrors( userData.errors )
+      else {
+        setErrors( null )
+        updateUser( userData )
+        history.push( '/' )
+      }
+  })
+    // Complete the post and test our '/signup' route 
+
+  }
     // 3.4 On a successful POST add the user to state (updateUser is passed down from app through props) and redirect to the Home page.
   // 4.✅ return to server/app.py to build the next route
  
     return (
         <> 
-        <h2 style={{ color:'red' }}> { 'Errors Here!!' }</h2>
+        <h2 style={{ color:'red' }}> {
+          errors ? errors.map( error => <h5>{ error }</h5> )
+          : null
+        }</h2>
         <h2>Please Log in or Sign up!</h2>
         <h2>{ signUp ? 'Already a member?' : 'Not a member?' }</h2>
         <button onClick = { handleClick }>{ signUp ? 'Log In!' : 'Register now!' }</button>
-        <Form onSubmit = { console.log }>
+        <Form onSubmit = { postToLoginOrSignup }>
         <label>
-          Email
+          Username
           </label>
-        <input type='text' name='email' value={ 'value' } onChange={ console.log } />
-        { signUp && (
+        <input type='text' name='username' value={ formState.username } onChange={ changeFormState } />
           <>
           <label>
           Password
           </label>
-          <input type='text' name='password' value={ 'value' } onChange={ console.log } />
+          <input type='password' name='password' value={ formState.password } onChange={ changeFormState } />
           </>
-        )}
         <input type='submit' value={ signUp ? 'Sign Up!' : 'Log In!' } />
       </Form>
         </>
--- 05-Auth-pt1/server/models.py
+++ 05-Auth-pt1/server/models.py
@@ -7,16 +7,13 @@
         #Rainbow Tables
     # Bcrypt
 
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
 
 # Let's move our flask_sqlalchemy import and the db line into our config file first to help clean up our code!
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
-
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.orm import validates
-
-# We should grab some imports from our config file, like db and bcrypt!
-
 
 # Time to work on creating a user model so we can have users login and use our app!
     # What are the other attributes we think a user should have?
@@ -29,16 +26,49 @@ class User(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # ✅ Add a column _password_hash
-        # Note: When an underscore is used, it's a sign that the variable or method is for internal use.
-    
-    # ✅ Create a hybrid_property that will protect the hash from being viewed
+    username = db.Column( db.String, nullable = False )
 
+    # ✅ Add a column _password_hash
+    _password_hash = db.Column( db.String, nullable = False )
+        # Note: When an underscore is used, it's a sign that the variable or method is for internal use.
+
+    serialize_rules = ( '-_password_hash', )
+    
+    validation_errors = []
+
+    @classmethod
+    def clear_validation_errors ( cls ) :
+        cls.validation_errors = []
+
+    # Password stuff for user model!
+    # ✅ Create a hybrid_property that will protect the hash from being viewed
+    @hybrid_property
+    def password_hash ( self ) :
+        return self._password_hash
+    
     # ✅ Create a setter method called password_hash that takes self and a password.
         # Use bcyrpt to generate the password hash with bcrypt.generate_password_hash
         # Set the _password_hash to the hashed password
+    @password_hash.setter
+    def password_hash ( self, password ) :
+        from app import bcrypt
+        if type( password ) is str and len( password ) in range( 6, 17 ) :
+            password_hash = bcrypt.generate_password_hash( password.encode( 'utf-8' ) )
+            self._password_hash = password_hash.decode( 'utf-8' )
+        else :
+            self.validation_errors.append( "Password must be between 6-16 characters long." )
 
     # ✅ Create an authenticate method that uses bcyrpt to verify the password against the hash in the DB with bcrypt.check_password_hash 
+    def authenticate ( self, password ) :
+        from app import bcrypt
+        return bcrypt.check_password_hash( self._password_hash, password.encode( 'utf-8' ) )
+    
+    @validates( 'username' )
+    def validate_username ( self, key, username ) :
+        if type( username ) is str and username :
+            return username
+        else :
+            self.validation_errors.append( 'Username cannot be blank.' )
 
     # ✅ Navigate to app
 
